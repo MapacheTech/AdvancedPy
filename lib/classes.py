c768022cd7f5dc9be651f9a744964a42eedf73e4
Isaac López Nolasco <122493130+isxaac21@users.noreply.github.com>
2024-01-11 08:30:02 -0600

    Primera clase

---
+++ lib/classes.py
@@ -0,0 +1,10 @@
+class Poligono:
+    def __init__(self, largo, ancho):
+        self.largo = largo
+        self.ancho = ancho
+        pass
+    
+    def __str__(self):
+        return f"Rectángulo de largo {self.largo} y ancho {self.ancho}"
+    
+    
